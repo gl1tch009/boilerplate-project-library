cd0ab7f21433c2d291936decef95dcf8712ea97d
Rares Groza <rares.groza@ascendro.de>
2023-12-02 02:38:34 +0200

    feat: add api logic, db connection and tests

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,102 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
+const { ObjectId } = require("mongodb");
 
-module.exports = function (app) {
+module.exports = function (app, db) {
+    app.route("/api/books")
+        .get(function (req, res) {
+            db.find({}).toArray((err, data) => {
+                if (err) {
+                    res.json({ error: err });
+                } else {
+                    res.json(data);
+                }
+            });
+            //response will be array of book objects
+            //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+        })
 
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
-    })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
-    });
+        .post(function (req, res) {
+            let title = req.body.title;
+            if (!title) {
+                return res.send("missing required field title");
+            }
+            db.insertOne(
+                { title: title, comments: [], commentcount: 0 },
+                (err, data) => {
+                    if (err) {
+                        res.json({ error: err });
+                    } else {
+                        res.json({ title: title, _id: data.insertedId });
+                    }
+                }
+            );
+            //response will contain new book object including atleast _id and title
+        })
 
+        .delete(function (req, res) {
+            db.deleteMany({}, (err, data) => {
+                if (err) {
+                    res.json({ error: err });
+                } else {
+                    res.send("complete delete successful");
+                }
+            });
+            //if successful response will be 'complete delete successful'
+        });
 
+    app.route("/api/books/:id")
+        .get(function (req, res) {
+            let bookid = req.params.id;
+            db.findOne({ _id: new ObjectId(bookid) }, (err, data) => {
+                if (!data) {
+                    res.send("no book exists");
+                } else {
+                    res.json({ ...data, comments: data.comments || [] });
+                }
+            });
+            //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+        })
 
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-    })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
-    })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
-    });
-  
+        .post(function (req, res) {
+            let bookid = req.params.id;
+            let comment = req.body.comment;
+            if (!comment) {
+                return res.send("missing required field comment");
+            }
+            db.findOneAndUpdate(
+                { _id: new ObjectId(bookid) },
+                { $push: { comments: comment }, $inc: { commentcount: 1 } },
+                (err, data) => {
+                    if (data.lastErrorObject.updatedExisting === false) {
+                        res.send("no book exists");
+                    } else {
+                        res.json({
+                            ...data.value,
+                            comments: [...data.value.comments, comment],
+                        });
+                    }
+                }
+            );
+            //json res format same as .get
+        })
+
+        .delete(function (req, res) {
+            let bookid = req.params.id;
+            db.deleteOne({ _id: new ObjectId(bookid) }, (err, data) => {
+                if (data.deletedCount === 0) {
+                    res.send("no book exists");
+                } else {
+                    res.send("delete successful");
+                }
+            });
+            //if successful response will be 'delete successful'
+        });
 };
--- server.js
+++ server.js
@@ -1,56 +1,60 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
+const myDB = require("./connection.js");
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: "*" })); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+myDB(async (client) => {
+    const db = await client.db("personal-library").collection("books");
 
-//For FCC testing purposes
-fccTestingRoutes(app);
+    //Index page (static HTML)
+    app.route("/").get(function (req, res) {
+        res.sendFile(process.cwd() + "/views/index.html");
+    });
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+    //For FCC testing purposes
+    fccTestingRoutes(app);
+
+    //Routing for API
+    apiRoutes(app, db);
+
+    //404 Not Found Middleware
+    app.use(function (req, res, next) {
+        res.status(404).type("text").send("Not Found");
+    });
+}).catch((e) => {
+    console.log(e);
 });
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
-      }
-    }, 1500);
-  }
+    console.log("Your app is listening on port " + listener.address().port);
+    if (process.env.NODE_ENV === "test") {
+        console.log("Running Tests...");
+        setTimeout(function () {
+            try {
+                runner.run();
+            } catch (e) {
+                console.log("Tests are not valid:");
+                console.error(e);
+            }
+        }, 1500);
+    }
 });
 
 module.exports = app; //for unit/functional testing
